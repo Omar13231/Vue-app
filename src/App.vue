b6f87b7a8fcb85d5356e4aa4e1b99af5556ae29a
Omar Harmouch <amigo1023329@gmail.com>
2023-08-13 17:35:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<section>
+    <h2> My friends </h2>
+    <ul>
+        <friend-contact></friend-contact>
+    </ul>
+</section>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            friends: [
+                {
+                    id: 'manuel',
+                    name : 'manuel lorenz',
+                    phone: '01827747378',
+                    email: 'joan@local.com'
+                },
+                {
+                    id: 'ferrera',
+                    name : 'hana ferrera',
+                    phone: '01827747378',
+                    email: 'hanah@local.com'
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2> {{ friend.name }}</h2>
+        <button  @click="toggleDetails">Show details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            detailsAreVisible: false,
+            friend: {
+                    id: 'manuel',
+                    name : 'manuel lorenz',
+                    phone: '01827747378',
+                    email: 'joan@local.com'
+                }
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+};
+</script>
